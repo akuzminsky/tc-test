67475c2605b37760a75ae34d5e10a3896aea4472
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-09-07 19:34:04 -0700

    Put instance in a default subnet

--- main.tf
+++ main.tf
@@ -14,9 +14,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_subnets" "current" {}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
+  subnet_id = data.aws_subnets.current.ids[0]
   instance_type = "t3.micro"
 
   tags = {
