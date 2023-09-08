e9fb538a2e3769cf6c29c963d14fecd29925ee32
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-09-07 17:56:56 -0700

    Add tc configuration

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "example-org-820a64"
+
+    workspaces {
+      name = "tc-test"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
